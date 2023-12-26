b6caac7a8f7516ec386af1fb4f0af6a487b837dd
Bruno Miranda <brunomiranda.data@gmail.com>
2023-12-26 00:53:07 -0300

    Functions created
    The display_subtitle, invalid_option, return_to_main_menu, register_new_restaurant and list_restaurants functions were created.

--- 01-Restaurant_app/restaurant_app.py
+++ 01-Restaurant_app/restaurant_app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurants = ['Pizza', 'Sushi']
+
 def display_program_name():
     print('EXPRESS RESTAURANT')
 
@@ -7,28 +9,60 @@ def display_options():
     print('1. Register restaurant')
     print('2. List restaurant')
     print('3. Activate restaurant')
-    print('4. Go out\n')
+    print('4. Exit\n')
 
 def finalize_app():
+    display_subtitle('Finish app')
+
+def return_to_main_menu():
+    input('\nEnter a key to return to the menu ')
+    main()
+
+def invalid_option():
+    print('Invalid option!\n')
+    return_to_main_menu()
+
+def display_subtitle(text):
     os.system('clear')
-    print('Finalizing the app')
+    print(text)
+    print()
 
-def choose_options():
-    chosen_option = int(input('Choose an option: '))
+def register_new_restaurant():
+    display_subtitle('Registration of new restaurants')
+    restaurant_name = input('Enter the name of the restaurant you wish to register: ')
+    restaurants.append(restaurant_name)
+    print(f'The restaurant {restaurant_name} was successfully registered!')
+    
+    return_to_main_menu()
 
-    if chosen_option == 1:
-        print('1. Register restaurant')
-    elif chosen_option == 2:
-        print('2. List restaurant')
-    elif chosen_option == 3:
-        print('3. Activate restaurant')
-    else:
-        finalize_app()
+
+def list_restaurants():
+    display_subtitle('Listing restaurants')
+    for restaurant in restaurants:
+        print(f'.{restaurant}')
+    return_to_main_menu()
+
+def chose_option():
+    try:
+        chosen_option = int(input('Choose an option: '))
+        if chosen_option == 1:
+            register_new_restaurant()
+        elif chosen_option == 2:
+            list_restaurants()
+        elif chosen_option == 3:
+            print('Activate restaurant')
+        elif chosen_option == 4:
+            finalize_app()
+        else:
+            invalid_option()
+    except:
+        invalid_option()
 
 def main():
+    os.system('clear')
     display_program_name()
     display_options()
-    choose_options()
+    chose_option()
 
 if __name__ == '__main__':
     main()
